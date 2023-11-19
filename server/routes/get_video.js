87c231b8c6514fed492fd23d736e4549e3449830
Leo Johnston <67394753+Daftscientist@users.noreply.github.com>
2023-11-19 01:06:43 +0000

    Update get_video.js

--- server/routes/get_video.js
+++ server/routes/get_video.js
@@ -1,18 +1,25 @@
+// --- External Imports ---
 const express = require('express');
-const router = express.Router();
 const axios = require('axios');
+
+// --- Internal Imports ---
 const { ytInitialData, videoData } = require('../libs/parsing');
 
-const YOUTUBE_VIDEO = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=)([^#\&\?]*).*/;
+// --- Persistant Instances ---
+const router = express.Router();
 
+// --- Globals ---
+const YOUTUBE_VIDEO = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=)([^#\&\?]*).*/;
 const reform_url = id => `https://www.youtube.com/watch?v=${id}`;
 
+// --- Route ---
 router.post('/', async (req, res, next) => {
     if (!req.body.url) {
         return res.status(400).send('No search term provided');
     }
 
     const match = YOUTUBE_VIDEO.exec(req.body.url);
+    
     if (!match || match[2].length !== 11) {
         return res.status(400).send('Invalid youtube video url');
     }
@@ -25,4 +32,5 @@ router.post('/', async (req, res, next) => {
     }
 });
 
+// --- Exports ---
 module.exports = router;
