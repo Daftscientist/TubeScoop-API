1eadc61ed9041e04920ac410ab040302035751c4
Leo Johnston <67394753+Daftscientist@users.noreply.github.com>
2023-11-19 01:00:27 +0000

    Update main.js

--- server/main.js
+++ server/main.js
@@ -1,20 +1,25 @@
+// --- External Packages ---
 const express = require('express');
+
+// --- Initializing ---
 const app = express();
 
-app.use(express.json()); // for parsing application/json
-app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+// --- Middleware ---
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-
+// --- Routes ---
 app.use('/search', require('./routes/search'));
 app.use('/get_video', require('./routes/get_video'));
 app.use('/get_playlist', require('./routes/get_playlist'));
 
-// Error handling middleware
+// --- Error Handling ---
 app.use((err, req, res, next) => {
     console.error(err.stack);
     res.status(500).send('Something broke!');
 });
 
+// --- HTTP Server ---
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
 });
